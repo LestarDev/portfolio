fc4dd73be5349ed0b29d966544d4d738ab0c751e
LestarDev <m.borkowski.2005@gmail.com>
2024-09-28 13:20:25 +0200

    some do into menu

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -18,7 +18,7 @@ const NavBar = ({setPage, currentPage}:{
             </div>
             <li>
                 <ChangePageButton pageName="Home" setPage={setPage} isPrimary={true} />
-                <input type="checkbox" name="menuCheckBox" id="menuCheckBox" />
+                <input type="checkbox" name="menuCheckBox" id="menuCheckBox" value={"false"} />
                 <label htmlFor="menuCheckBox">
                     <span style={{"--menu-position": "1"} as React.CSSProperties}></span>
                     <span className="middle" style={{"--menu-position": "0"} as React.CSSProperties}></span>
